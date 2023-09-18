f377b267d43be8dff265151eb0a94b4eb0a87ad8
devin-hale <devinwaynehale@gmail.com>
2023-09-18 09:45:00 -0500

    Updated nodemailer util to TS

--- controllers/utils/nodeMailer.ts
+++ controllers/utils/nodeMailer.ts
@@ -1,19 +1,19 @@
-import nodemailer from "nodemailer";
-import "dotenv/config.js";
+import nodemailer from 'nodemailer';
+import 'dotenv/config.js';
 
 const transporter = nodemailer.createTransport({
-	service: "gmail",
+	service: 'gmail',
 	auth: {
 		user: process.env.MAIL,
 		pass: process.env.MAILPASS,
 	},
 });
 
-const verifyOptions = (userEmail, authCode) => {
+const verifyOptions = (userEmail:any, authCode:any) => {
 	return {
 		from: process.env.MAIL,
 		to: userEmail,
-		subject: "Verify Your school.io Email!",
+		subject: 'Verify Your school.io Email!',
 		text: `Welcome to school.io! \n
             Here is your account verification code: \n
             ${authCode} \n
@@ -21,14 +21,14 @@ const verifyOptions = (userEmail, authCode) => {
 	};
 };
 
-const sendVerification = async (userEmail, authCode) => {
-	await transporter.sendMail(
+const sendVerification = async (userEmail: any, authCode: any) => {
+	transporter.sendMail(
 		verifyOptions(userEmail, authCode),
 		function (error, info) {
 			if (error) {
 				console.log(error);
 			} else {
-				console.log("Email sent:" + info.response);
+				console.log('Email sent:' + info.response);
 			}
 		}
 	);
